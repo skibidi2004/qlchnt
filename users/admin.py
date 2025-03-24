1437afd043946e9e88310f1a3ba8992c7a196032
Vydangiu <ngothaovy09@gmail.com>
2025-03-24 12:59:47 +0700

    admin quan ly user voi store

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ("username", "email", "role", "phone", "is_staff", "is_superuser")
+    fieldsets = UserAdmin.fieldsets + (
+        ("Custom Fields", {"fields": ("role", "phone")}),
+    )
+
+admin.site.register(User, CustomUserAdmin)
+
